57f9d8cb42c70e3e2720727e59c3f27ce25eed71
David Li <dtli@lcs.on.ca>
2023-04-20 10:04:45 -0400

    Initial Commit

---
+++ CurrencyExchange/CurrencyExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyExchangeApp.swift
+//  CurrencyExchange
+//
+//  Created by David Li on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
